6e092d5c2c7b8bc50a52888788a44116b50a6cb3
Henry Catalini Smith <henry@catalinismith.com>
2015-05-10 22:15:33 +0100

    add some more

--- lib/bad_regex.rb
+++ lib/bad_regex.rb
@@ -8,6 +8,9 @@ module BadRegex
     /(p|Lar*y\s+Ell)i.*/,
     /[ a-yLE]{3,18}/,
     /([pl])([ easyrio]+)([en])/i,
+    /[ a-yLE]{3,18}/,
+    /\u0070*([ALL][day][ridge][racer][yesYES ]{3}..){0,13}[i](?:\x65|(?:[sister][mother][uncle]))/,
+    /[pretty much anything really, fucking hell]+/,
   ]
 
   def self.patterns
